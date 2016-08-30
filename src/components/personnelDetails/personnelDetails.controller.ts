97fa5bf0e2a1bc8b4d66d6b11b0db9a2b0512291
Kirk Campbell <kirk93@gmail.com>
2016-08-30 23:11:52 +0800

    Add personnel details screen

--- src/components/personnelDetails/personnelDetails.controller.ts
+++ src/components/personnelDetails/personnelDetails.controller.ts
@@ -11,6 +11,11 @@ class PersonnelDetailsController {
     this.emergencyContacts.push(this.selected.emergency1);
     this.emergencyContacts.push(this.selected.emergency2);
   }
+
+  public call(phoneNum: string) {
+    console.log('hello');
+    window.location.href= "tel: " + phoneNum;
+  }
 }
 
 angular.module('resource-mobile').controller('PersonnelDetailsController', PersonnelDetailsController);
