9e4bbf41f805bc1b778afa9d00740d4e648b8e90
Kirk Campbell <kirk93@gmail.com>
2016-09-11 15:58:51 +0800

    Issue #1 - Style login page

--- src/components/login/login.controller.ts
+++ src/components/login/login.controller.ts
@@ -7,6 +7,8 @@ class LoginCtrl {
 
   private loginForm: LoginForm;
 
+  private loggingIn: boolean;
+
   private error: boolean;
 
   static $inject = ['$scope', '$http', 'LoginService', 'LoginModalService'];
@@ -15,9 +17,16 @@ class LoginCtrl {
     private loginService: LoginService,
     private loginModal: LoginModalService) { 
       this.error = false;
+      this.loggingIn = false;
+
+      window.addEventListener('native.keyboardshow', function(){
+        // Ionic delays this by default
+        document.body.classList.add('keyboard-open');
+      });
     }
 
   doLogin() {
+    this.loggingIn = true;
     this.loginService.logIn(this.loginForm).then((user: User) => {
       if (user) {
         this.loginModal.remove();
@@ -26,6 +35,7 @@ class LoginCtrl {
         this.error = true;
         this.loginForm.password = "";
       }
+      this.loggingIn = false;
     });
   }
 }
